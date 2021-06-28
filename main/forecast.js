26aed5048b7f0a1a1bf80cfc6882d4d34db1ef41
anandhuks <64020980+anandhukss@users.noreply.github.com>
2021-06-28 13:43:30 +0530

    Update forecast.js

--- main/forecast.js
+++ main/forecast.js
@@ -1,6 +1,6 @@
 const http = require('request')
 const forecast = (city, callback) => {
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(city) + "&appid={ api key}"+"&units=metric";
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(city) + "&appid=a4487ed8466a5926b462a78ddeaa0363"+"&units=metric";
     http({ url: url, json: true }, (errorMessage, response) => {
         if (errorMessage) {
             callback("Unable to connect to weather service", undefined)
